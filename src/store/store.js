61a461a51d01455e3dbf728c5c58b621e2679f6b
modeverv <modeverv@gmail.com>
2018-06-27 01:29:03 +0900

    2018-06-27 01:29:01

--- src/store/store.js
+++ src/store/store.js
@@ -13,7 +13,7 @@ const state = {
 
 function getGIFs(query) {
   const params = encodeURIComponent(query).replace(/%20/g, '+')
-  return fetch('http://api.giphy.com/v1/gifs/search?q=' + params + '&api_key=dc6zaTOxFJmzC')
+  return fetch('https://api.giphy.com/v1/gifs/search?q=' + params + '&api_key=dc6zaTOxFJmzC')
     .then(res => res.json())
 }
 
